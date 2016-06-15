24766efd9c1420047d370c119d5f05c9050488a6
sammylan <sammylan@gmail.com>
2016-06-15 10:46:31 +0800

    CreateMProcess

--- CPPConsole/System/CreateProcessHelper.cpp
+++ CPPConsole/System/CreateProcessHelper.cpp
@@ -1,12 +1,14 @@
 #include "stdafx.h"
-
 #include "CreateProcessHelper.h"
 #include <Sddl.h>
+#include <atlsecurity.h>
 
 namespace System
 {
 	BOOL SetIntegrity(HANDLE hToken, LPCTSTR level)
 	{
+		//https://support.microsoft.com/en-hk/kb/243330
+		//http://forcemz.net/container/2015/06/12/AppContainer/
 		PSID pIntegritySid = nullptr;
 		BOOL bRet = ConvertStringSidToSid(level, &pIntegritySid);
 		if (bRet)
@@ -25,6 +27,22 @@ namespace System
 
 	BOOL CreateMProcess(LPCWSTR lpCmdLine)
 	{
+		CAccessToken hProcess;
+		hProcess.GetProcessToken(TOKEN_DUPLICATE);
+		CAccessToken hDupToken;
+		HANDLE hTmp;
+		DuplicateTokenEx(hProcess.GetHandle(), MAXIMUM_ALLOWED, 0, SecurityAnonymous, TokenPrimary,&hTmp);
+		hDupToken.Attach(hTmp);
+
+		SetIntegrity(hTmp, Integrity_Level::Medium);
+
+		STARTUPINFO  si;
+		ZeroMemory(&si, sizeof(si));
+		si.cb = sizeof(si);
+
+		PROCESS_INFORMATION pi;
+		ZeroMemory(&pi, sizeof(pi));
+		hDupToken.CreateProcessAsUser(lpCmdLine, NULL, &pi, &si, CREATE_UNICODE_ENVIRONMENT, false, NULL, NULL, TRUE);
 		return TRUE;
 	}
 }
--- CPPConsole/System/CreateProcessHelper.h
+++ CPPConsole/System/CreateProcessHelper.h
@@ -6,9 +6,9 @@ namespace System
 	namespace Integrity_Level
 	{
 		static TCHAR const * const Low = _T("S-1-16-4096");
-		static TCHAR const * const Medium = _T("S-1-16-4096");
-		static TCHAR const * const High = _T("S-1-16-4096");
-		static TCHAR const * const System = _T("S-1-16-4096");
+		static TCHAR const * const Medium = _T("S-1-16-8192");
+		static TCHAR const * const High = _T("S-1-16-12288");
+		static TCHAR const * const System = _T("S-1-16-16384");
 	}
 	BOOL SetIntegrity(HANDLE hToken, LPCTSTR level);
 	BOOL CreateMProcess(LPCWSTR lpCmdLine);
