d5f094f0fd84e1d99f2dde8c961216d9dd47cb51
sammylan <sammylan@gmail.com>
2016-06-15 12:39:32 +0800

    GetRemoveAdminToken

--- CPPConsole/System/CreateProcessHelper.cpp
+++ CPPConsole/System/CreateProcessHelper.cpp
@@ -25,24 +25,61 @@ namespace System
 		return bRet;
 	}
 
+	BOOL GetRemoveAdminToken(CAccessToken & hToken)
+	{
+		if (hToken.GetHandle() != NULL)
+		{
+			CloseHandle(hToken.Detach());
+		}
+
+		//获取当前进程的Token
+		CAccessToken hProcessToken;
+		hProcessToken.GetProcessToken(TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE |
+			TOKEN_QUERY | TOKEN_ADJUST_DEFAULT);
+
+		//PSID可变长度的数据结构
+		BYTE sidBuffer[256] = { 0 };
+		PSID pAdminSID = (PSID)sidBuffer;
+		SID_IDENTIFIER_AUTHORITY SIDAuth = SECURITY_NT_AUTHORITY;
+		if (!AllocateAndInitializeSid(&SIDAuth, 2,
+			SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS,
+			0, 0, 0, 0, 0, 0, &pAdminSID))
+		{
+			printf("AllocateAndInitializeSid Error%u\n", GetLastError());
+			return FALSE;
+		}
+
+		SID_AND_ATTRIBUTES SidToDisable[1] = { 0 };
+		SidToDisable[0].Sid = pAdminSID;
+		SidToDisable[0].Attributes = 0;
+
+		HANDLE hNewToken = NULL;
+		if (!CreateRestrictedToken(hProcessToken.GetHandle(),
+			DISABLE_MAX_PRIVILEGE,
+			1, SidToDisable,
+			0, NULL,0, NULL,
+			&hNewToken))
+		{
+			printf("CreateRestrictedToken failed(%lu)\n", GetLastError());
+			return FALSE;
+		}
+		hToken.Attach(hNewToken);
+		return hNewToken != NULL;
+	}
+
 	BOOL CreateMProcess(LPCWSTR lpCmdLine)
 	{
-		CAccessToken hProcess;
-		hProcess.GetProcessToken(TOKEN_DUPLICATE);
-		CAccessToken hDupToken;
-		HANDLE hTmp;
-		DuplicateTokenEx(hProcess.GetHandle(), MAXIMUM_ALLOWED, 0, SecurityAnonymous, TokenPrimary,&hTmp);
-		hDupToken.Attach(hTmp);
-
-		SetIntegrity(hTmp, Integrity_Level::Medium);
+		CAccessToken hToken;
+		GetRemoveAdminToken(hToken);
+		SetIntegrity(hToken.GetHandle(), Integrity_Level::Medium);
 
 		STARTUPINFO  si;
 		ZeroMemory(&si, sizeof(si));
 		si.cb = sizeof(si);
 
 		PROCESS_INFORMATION pi;
-		ZeroMemory(&pi, sizeof(pi));
-		hDupToken.CreateProcessAsUser(lpCmdLine, NULL, &pi, &si, CREATE_UNICODE_ENVIRONMENT, false, NULL, NULL, TRUE);
+		ZeroMemory(&pi, sizeof(pi));		
+		hToken.CreateProcessAsUser(lpCmdLine, NULL, &pi, &si, CREATE_UNICODE_ENVIRONMENT, false, NULL, NULL, TRUE);
 		return TRUE;
 	}
 }
